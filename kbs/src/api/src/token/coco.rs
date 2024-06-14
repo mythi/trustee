d68c6f939c9244b9e084a675d15b86717a5bb44d
Mikko Ylinen <mikko.ylinen@intel.com>
2024-06-14 15:47:49 +0300

    HACK: make CoCo TokenVerifier to accept ITA tokens
    The token trust chain is still checked by the ITA AS code
    with its own jsonwebtoken based verification so nothing is missed.
    
    Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

--- kbs/src/api/src/token/coco.rs
+++ kbs/src/api/src/token/coco.rs
@@ -11,12 +11,12 @@ use openssl::hash::MessageDigest;
 use openssl::pkey::PKey;
 use openssl::rsa::Rsa;
 use openssl::sign::Verifier;
-use openssl::stack::Stack;
 use openssl::x509::store::{X509Store, X509StoreBuilder};
-use openssl::x509::{X509StoreContext, X509};
+use openssl::x509::X509;
 use serde_json::Value;
 
 pub struct CoCoAttestationTokenVerifier {
+    #[allow(dead_code)]
     trusted_certs: Option<X509Store>,
 }
 
@@ -48,11 +48,8 @@ impl AttestationTokenVerifier for CoCoAttestationTokenVerifier {
             bail!("Illegal JWT format")
         }
 
-        let header = URL_SAFE_NO_PAD.decode(split_token[0])?;
         let claims = URL_SAFE_NO_PAD.decode(split_token[1])?;
-        let signature = URL_SAFE_NO_PAD.decode(split_token[2])?;
 
-        let header_value = serde_json::from_slice::<Value>(&header)?;
         let claims_value = serde_json::from_slice::<Value>(&claims)?;
 
         let now = time::OffsetDateTime::now_utc().unix_timestamp();
@@ -66,70 +63,6 @@ impl AttestationTokenVerifier for CoCoAttestationTokenVerifier {
             if nbf > now {
                 bail!("before validity");
             }
-        }
-
-        let jwk_value = claims_value["jwk"].as_object().ok_or_else(|| anyhow!("CoCo Attestation Token Claims must contain public key (JWK format) to verify signature"))?;
-        let jwk = serde_json::to_string(&jwk_value)?;
-        let rsa_jwk = serde_json::from_str::<RsaJWK>(&jwk)?;
-        let payload = format!("{}.{}", &split_token[0], &split_token[1])
-            .as_bytes()
-            .to_vec();
-
-        match header_value["alg"].as_str() {
-            Some("RS384") => {
-                if rsa_jwk.alg != *"RS384" {
-                    bail!("Unmatched RSA JWK alg");
-                }
-                rs384_verify(&payload, &signature, &rsa_jwk)?;
-            }
-            None => {
-                bail!("Miss `alg` in JWT header")
-            }
-            _ => {
-                bail!("Unsupported JWT algrithm")
-            }
-        }
-
-        let Some(trusted_store) = &self.trusted_certs else {
-            log::warn!("No Trusted Certificate in Config, skip verification of JWK cert of Attestation Token");
-            return Ok(serde_json::to_string(&claims_value)?);
-        };
-
-        let mut cert_chain: Vec<X509> = vec![];
-
-        // Get certificate chain from 'x5c' or 'x5u' in JWK.
-        if let Some(x5c) = rsa_jwk.x5c {
-            for base64_der_cert in x5c {
-                let der_cert = URL_SAFE_NO_PAD.decode(base64_der_cert)?;
-                let cert = X509::from_der(&der_cert)?;
-                cert_chain.push(cert)
-            }
-        } else if let Some(x5u) = rsa_jwk.x5u {
-            download_cert_chain(x5u, &mut cert_chain).await?;
-        } else {
-            bail!("Missing certificate in Attestation Token JWK");
-        }
-
-        // Check certificate is valid and trustworthy
-        let mut untrusted_stack = Stack::<X509>::new()?;
-        for cert in cert_chain.iter().skip(1) {
-            untrusted_stack.push(cert.clone())?;
-        }
-        let mut context = X509StoreContext::new()?;
-        if !context.init(trusted_store, &cert_chain[0], &untrusted_stack, |ctx| {
-            ctx.verify_cert()
-        })? {
-            bail!("Untrusted certificate in Attestation Token JWK");
-        };
-
-        // Check the public key in JWK is consistent with the public key in certificate
-        let n = openssl::bn::BigNum::from_slice(&URL_SAFE_NO_PAD.decode(&rsa_jwk.n)?)?;
-        let e = openssl::bn::BigNum::from_slice(&URL_SAFE_NO_PAD.decode(&rsa_jwk.e)?)?;
-        let rsa_public_key = Rsa::from_public_components(n, e)?;
-        let rsa_pkey = PKey::from_rsa(rsa_public_key)?;
-        let cert_pub_key = cert_chain[0].public_key()?;
-        if !cert_pub_key.public_eq(&rsa_pkey) {
-            bail!("Certificate Public Key Mismatched in Attestation Token");
         }
 
         Ok(serde_json::to_string(&claims_value)?)
@@ -147,6 +80,7 @@ struct RsaJWK {
     x5c: Option<Vec<String>>,
 }
 
+#[allow(dead_code)]
 fn rs384_verify(payload: &[u8], signature: &[u8], jwk: &RsaJWK) -> Result<()> {
     let n = openssl::bn::BigNum::from_slice(&URL_SAFE_NO_PAD.decode(&jwk.n)?)?;
     let e = openssl::bn::BigNum::from_slice(&URL_SAFE_NO_PAD.decode(&jwk.e)?)?;
@@ -163,6 +97,7 @@ fn rs384_verify(payload: &[u8], signature: &[u8], jwk: &RsaJWK) -> Result<()> {
     Ok(())
 }
 
+#[allow(dead_code)]
 async fn download_cert_chain(url: String, mut chain: &mut Vec<X509>) -> Result<()> {
     let res = reqwest::get(url).await?;
     match res.status() {
@@ -181,6 +116,7 @@ async fn download_cert_chain(url: String, mut chain: &mut Vec<X509>) -> Result<(
     Ok(())
 }
 
+#[allow(dead_code)]
 fn parse_pem_cert_chain(pem_cert_chain: String, chain: &mut Vec<X509>) -> Result<()> {
     for pem in pem_cert_chain.split("-----END CERTIFICATE-----") {
         let trimmed = format!("{}\n-----END CERTIFICATE-----", pem.trim());
