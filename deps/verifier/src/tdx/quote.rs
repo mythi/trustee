cdf9d28e1885d74686c2e45ad470c062db5f2b3f
Mikko Ylinen <mikko.ylinen@intel.com>
2025-11-05 09:55:16 +0200

    WIP: implement PCK Certificate parsing
    Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

--- deps/verifier/src/tdx/quote.rs
+++ deps/verifier/src/tdx/quote.rs
@@ -1,14 +1,30 @@
 use crate::sgx::types::*;
 use anyhow::{anyhow, bail, Context, Result};
+use asn1_rs::{oid, DerSequence, Enumerated, FromDer, Oid};
 use core::fmt;
 use openssl::bn::BigNum;
 use openssl::nid::Nid;
+use openssl::sha::sha256;
 use openssl::stack::Stack;
-use openssl::x509::{self, store::X509StoreBuilder, X509StoreContext, X509};
+use openssl::x509::{
+    self, store::X509Store, store::X509StoreBuilder, X509StoreContext, X509VerifyResult, X509,
+};
 use openssl::{ec::EcGroup, ec::EcKey, ecdsa::EcdsaSig};
 use scroll::Pread;
+use std::sync::LazyLock;
+use x509_parser::prelude::X509Certificate;
 
 pub const QUOTE_HEADER_SIZE: usize = 48;
+
+static INTEL_PCS_ROOT_CA: LazyLock<X509Store> = LazyLock::new(|| {
+    let trust_anchor = X509::from_pem(include_bytes!(
+        "Intel_SGX_Provisioning_Certification_RootCA.pem"
+    ))
+    .unwrap();
+    let mut builder = X509StoreBuilder::new().unwrap();
+    builder.add_cert(trust_anchor).unwrap();
+    builder.build()
+});
 
 /// The quote header. It is designed to compatible with earlier versions of the quote.
 #[repr(C)]
@@ -406,6 +422,82 @@ fn parse_certification_data(data: &[u8]) -> Result<QuoteSignatureData> {
     })
 }
 
+const DCAP_SGX_EXTENSIONS: Oid<'static> = oid!(1.2.840 .113741 .1 .13 .1);
+
+#[derive(Debug, PartialEq, DerSequence)]
+struct OidAndString<'ext> {
+    id: Oid<'ext>,
+    s: &'ext [u8],
+}
+
+#[derive(Debug, PartialEq, DerSequence)]
+struct OidAndInt<'ext> {
+    id: Oid<'ext>,
+    val: u8,
+}
+
+#[derive(Debug, PartialEq, DerSequence)]
+struct OidAndEnum<'ext> {
+    id: Oid<'ext>,
+    e: Enumerated,
+}
+
+#[derive(Debug, PartialEq, DerSequence)]
+struct OidAndBool<'ext> {
+    id: Oid<'ext>,
+    b: bool,
+}
+
+#[derive(Debug, PartialEq, DerSequence)]
+struct PlatformConfig<'ext> {
+    dynamic_platform: OidAndBool<'ext>,
+    cached_keys: OidAndBool<'ext>,
+    smt_enabled: OidAndBool<'ext>,
+}
+
+#[derive(Debug, PartialEq, DerSequence)]
+struct ConfigSequence<'ext> {
+    id: Oid<'ext>,
+    configs: PlatformConfig<'ext>,
+}
+
+#[derive(Debug, PartialEq, DerSequence)]
+struct Tcbs<'ext> {
+    comp1: OidAndInt<'ext>,
+    comp2: OidAndInt<'ext>,
+    comp3: OidAndInt<'ext>,
+    comp4: OidAndInt<'ext>,
+    comp5: OidAndInt<'ext>,
+    comp6: OidAndInt<'ext>,
+    comp7: OidAndInt<'ext>,
+    comp8: OidAndInt<'ext>,
+    comp9: OidAndInt<'ext>,
+    comp10: OidAndInt<'ext>,
+    comp11: OidAndInt<'ext>,
+    comp12: OidAndInt<'ext>,
+    comp13: OidAndInt<'ext>,
+    comp14: OidAndInt<'ext>,
+    comp15: OidAndInt<'ext>,
+    comp16: OidAndInt<'ext>,
+    pcesvn: OidAndInt<'ext>,
+    cpusvn: OidAndString<'ext>,
+}
+#[derive(Debug, PartialEq, DerSequence)]
+struct TcbSequence<'ext> {
+    id: Oid<'ext>,
+    tcbs: Tcbs<'ext>,
+}
+#[derive(Debug, PartialEq, DerSequence)]
+struct SgxExtension<'ext> {
+    ppid: OidAndString<'ext>,
+    tcb: TcbSequence<'ext>,
+    pceid: OidAndString<'ext>,
+    fmspc: OidAndString<'ext>,
+    sgxtype: OidAndEnum<'ext>,
+    platform_instance: OidAndString<'ext>,
+    configuration: ConfigSequence<'ext>,
+}
+
 pub fn parse_tdx_quote(quote_bin: &[u8]) -> Result<Quote> {
     let quote_header = &quote_bin[..QUOTE_HEADER_SIZE];
     let header = quote_header
@@ -491,6 +583,55 @@ pub fn parse_tdx_quote(quote_bin: &[u8]) -> Result<Quote> {
                                 Ok(())
                             }
                         })?;
+
+                    if qe.qe_certification_data.certificates[2]
+                        .issued(&qe.qe_certification_data.certificates[2])
+                        != X509VerifyResult::OK
+                    {
+                        println!("{:?}", qe.qe_certification_data.certificates[2]);
+                    }
+
+                    if qe.qe_certification_data.certificates[2]
+                        .issued(&qe.qe_certification_data.certificates[1])
+                        != X509VerifyResult::OK
+                    {
+                        println!("{:?}", qe.qe_certification_data.certificates[1]);
+                    }
+
+                    if qe.qe_certification_data.certificates[1]
+                        .issued(&qe.qe_certification_data.certificates[0])
+                        != X509VerifyResult::OK
+                    {
+                        println!("{:?}", qe.qe_certification_data.certificates[0]);
+                    }
+
+                    let pck_der = qe.qe_certification_data.certificates[0].to_der()?;
+                    let parsed_pck = X509Certificate::from_der(&pck_der)?.1.tbs_certificate;
+
+                    let value = parsed_pck
+                        .get_extension_unique(&DCAP_SGX_EXTENSIONS)?
+                        .ok_or_else(|| anyhow!("SGX Extensions not found in PCK Cert"))?
+                        .value;
+
+                    let (_, parsed) = SgxExtension::from_der(value).expect("Failed to parse");
+                    println!("Parsed: {:?}", parsed);
+                    println!("Parsed: {:?}", hex::encode(parsed.fmspc.s));
+
+                    let mut intermediate_certs = Stack::<X509>::new()?;
+                    intermediate_certs.push(qe.qe_certification_data.certificates[1].clone())?;
+                    let mut context = X509StoreContext::new()?;
+                    let verified = context
+                        .init(
+                            &INTEL_PCS_ROOT_CA,
+                            &qe.qe_certification_data.certificates[0],
+                            &intermediate_certs,
+                            |c| c.verify_cert(),
+                        )
+                        .map_err(|e| anyhow!(e.to_string()))?;
+
+                    if !verified {
+                        bail!("Report certificate chain failed to verify");
+                    }
 
                     Ok(Quote::V5 {
                         header,
